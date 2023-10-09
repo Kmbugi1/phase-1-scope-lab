7799a31deb90850126f8deb3b6904b04cf2dc6fe
keith <keith.wachira@students.moringaschol.com>
2023-10-09 11:18:55 +0300

    scopes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+var customerName;
+function upperCaseCutomerName() {
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+    }
+}
+function setBestCustomer() {
+    bestCustomer ='not bob';
+}
+function overwriteBestCustomer() {
+    setBestCustomer ='still not bob';
+}
+const leastFavoriteCustomer = 'john doe'
